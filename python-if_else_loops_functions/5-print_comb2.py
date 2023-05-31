4827977528bd8dc4262e89505037cf7ea9457016
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-05-31 09:03:22 -0700

    Task 05

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
     if i != 99:
-        print("%02d, " % (i,), end="")
+        print("%02d, " % (i), end="")
     else:
-        print("%02d" % (i,), end="\n")
+        print("%02d" % (i))
