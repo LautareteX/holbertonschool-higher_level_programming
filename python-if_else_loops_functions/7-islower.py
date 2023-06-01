e39ae95fa1f6fd0279e8ee5aea7b72f33699415d
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-01 13:45:32 -0700

    Stressful

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def islower(c):
     if ord(c) in range(96, 123):
         return True
     else:
         return False
-    
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
         letter = ord(str[i])
