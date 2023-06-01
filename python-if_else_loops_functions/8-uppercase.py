3f594396b96aebdceaa04ce3b67cac2916bd8837
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-01 09:25:33 -0700

    Upper

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in range(len(str)):
+        letter = ord(str[i])
+        if letter in range(96, 123):
+            letter -= 32
+            print("{}".format(chr(letter)), end="")
+        else:
+            letter = letter
+            print("{}".format(chr(letter)), end="")
+    print()
