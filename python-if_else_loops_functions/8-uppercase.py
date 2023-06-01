8851275a3ee452dbcef9e9ded17877c68388e3fa
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-01 13:50:59 -0700

    More easy solution

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        letter = ord(str[i])
-        if letter in range(96, 123):
-            letter -= 32
-            print("{}".format(chr(letter)), end="")
-        else:
-            letter = letter
-            print("{}".format(chr(letter)), end="")
+    for i in str:
+        i = ord(i)
+        if i in range(96, 123):
+            i -= 32
+        print("{}".format(chr(i)), end="")
     print()
