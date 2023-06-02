de6794b3d577d707ff20eca4c66ce3d8086079fb
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-02 16:38:38 -0700

    Task 01

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from add_0 import add #Desde el archivo add_0 importar la funcion add
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
