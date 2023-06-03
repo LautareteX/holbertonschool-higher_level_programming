5d417b4b41df0d2b03644124645a946709617980
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-03 11:30:25 -0700

    IT Task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 import sys
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
-    print("{} arguments.".format(argc))
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    if argc == 1:
+        print("1 argument:")
+    elif argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments.".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
