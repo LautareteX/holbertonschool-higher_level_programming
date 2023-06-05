043ccf2af3d7c03fbb5fc8cc0d1f9f2d5c424bdd
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-05 08:58:13 -0700

    Fixed style

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    
+
     new_list = my_list
 
     for i in my_list:
         if idx > len(my_list) - 1 or idx < 0:
             return my_list
-        
+
         if my_list[i] == my_list[idx]:
             new_list[i] = element
             return new_list
