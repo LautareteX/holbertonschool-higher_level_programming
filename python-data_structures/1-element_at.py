dc0b18b3d7ee2b0eb84a93f3be3c08ea0360796b
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-05 08:06:48 -0700

    New try

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 
-    if idx < 0:
-        return None
-
     for i in my_list:
-        if idx > len(my_list) - 1:
+        if idx > len(my_list) - 1 or idx < 0:
             return None
         if my_list[i] == my_list[idx]:
             return my_list[i]
