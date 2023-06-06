8bd9135bd1781d679aab882f510d526642e50c80
Lautaro Rodriguez <81494428+LautareteX@users.noreply.github.com>
2023-06-06 15:28:44 -0300

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
-    for i in matrix:
-        print(' '.join(map("{}".format, i)))
+    for value in matrix:
+        for i, j in enumerate(value, start=1):
+            if i != 1:
+                print("{}".format(" "), end="")
+            print("{:d}".format(j), end="")
+        print()
