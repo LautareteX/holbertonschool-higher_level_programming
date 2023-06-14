bfcf63c4519c7ef56e784ab801e3f8bc606c6315
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-14 09:25:57 -0700

    Task 003

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Create class square"""
+
+
+class Square:
+    """Define a square"""
+    def __init__(self, size=0) -> None:
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
+        pass
+
+    def area(self):
+        return (self.__size ** 2)
