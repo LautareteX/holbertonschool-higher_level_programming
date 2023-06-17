b13b961337c68a83b5f6323f0cae5dcade7a944a
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-17 12:24:39 -0700

    Task 01

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+def add_integer(a, b=98):
+    """
+    Testing sum function
+    >>> add_integer(5, 5)
+    10
+    >>> add_integer(-1, 1)
+    0
+    """
+
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
