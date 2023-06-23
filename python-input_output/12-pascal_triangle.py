6607d09431243a08b33fa15d64fafee8ee801198
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-23 12:42:49 -0700

    Task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Pascal triangle without fibonacci
+"""
+
+
+def pascal_triangle(n):
+    my_list = []
+
+    if n <= 0:
+        return my_list
+    else:
+        for i in range(n):
+            res = 11**i
+            my_list.append(str(res))
+        return my_list
