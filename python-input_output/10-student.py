0d28b565288c2b14b8cb6908fc3a441792d1d946
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-23 12:35:03 -0700

    new stdt

---
+++ python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+returns dictionary description
+"""
+
+
+class Student():
+    """
+    student information for
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """New student rep"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to json file rep"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+    
+        new_dict = {}
+        for i in attrs:
+            if i in self.__dict__.keys():
+                new_dict[i] = self.__dict__[i]
+        return new_dict
