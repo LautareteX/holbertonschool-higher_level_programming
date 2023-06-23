2f098beb93c5326142b8f9fdce4750009b01f3b9
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-23 07:56:15 -0700

    Task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,12 +5,6 @@ Python Reading file
 
 
 def read_file(filename=""):
-    """
-    reading and printing content
-    """
-    if filename:
-        with open(filename, encoding="UTF8") as f:
-            read_data = f.read()
-
-        print(read_data)
-        f.close
+    """ reading and printing content """
+    with open(filename, "r", encoding="UTF8") as f:
+        print(f.read(), end="")
