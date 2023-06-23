b1ffcef807f0e439331ab5b20fce031ea888c512
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-23 03:40:53 -0700

    Task 06

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Empty class
+"""
+
+
+class BaseGeometry:
+    """
+    My Geometry :)
+    """
+    pass
+    def  area(self):
+        raise Exception("area() is not implemented")
