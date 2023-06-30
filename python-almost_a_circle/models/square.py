7e90b14b37d4983e767403810563ceb3054585c2
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-30 09:15:22 -0700

    Function commented

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,11 +6,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Class square that inherits from Rectangle"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """Constructor clase"""
+        """Constructor class"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Return the string representation of the square"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - " \
-            f"{self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
