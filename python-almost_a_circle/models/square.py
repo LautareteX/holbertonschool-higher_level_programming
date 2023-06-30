51996b96b64e761ca42d45776c49354873e4822c
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-06-30 12:36:04 -0700

    Task 11 #Square Size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -15,3 +15,40 @@ class Square(Rectangle):
     def __str__(self):
         """Return the string representation of the square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self._Rectangle__width
+
+    @property
+    def x(self):
+        return self._Rectangle__x
+
+    @property
+    def y(self):
+        return self._Rectangle__y
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value <= 0:
+            raise ValueError("size must be > 0")
+        self._Rectangle__width = value
+        self._Rectangle__height = value
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self._Rectangle__y = value
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self._Rectangle__x = value
