675b9dd9ae3fa35d909aefa717b430f3d01d0cd6
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-09-19 02:45:56 -0700

    More and more and more and more if

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+if number <= 0:
+    if last_digit == 0:
+        print("Last digit of {} is {} and is less than 6 and not 0"
+              .format(number, last_digit))
+    else:
+        print("Last digit of {} is -{} and is less than 6 and not 0"
+              .format(number, last_digit))
+else:
+    if last_digit > 5:
+        print("Last digit of {} is {} and greater than 5"
+              .format(number, last_digit))
+    elif last_digit == 0:
+        print("Last digit of {} is {} and is zero"
+              .format(number, last_digit))
+    elif last_digit < 6:
+        print("Last digit of {} is {} and is less than 6 and not 0"
+              .format(number, last_digit))
