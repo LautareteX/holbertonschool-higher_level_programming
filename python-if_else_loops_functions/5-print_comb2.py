baa90beca7ac1b7b94cda511960ee88c674d177f
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-09-20 07:10:22 -0700

    task5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-    print("{}, ".format(i), end="")
+    if i < 10:
+        print("0{}, ".format(i), end="")
+    else:
+        print("{}, ".format(i), end="")
 print(99)
