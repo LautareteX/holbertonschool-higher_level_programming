0a66c03c1d2d483617b510743332077b19eecf7f
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-09-23 16:45:50 -0700

    Task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,8 +3,13 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
+    
+    result_add = add(a, b)
+    result_subtract = sub(a, b)
+    result_multiply = mul(a, b)
+    result_divide = div(a, b)
 
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print(f"{a} + {b} = {result_add}")
+    print(f"{a} - {b} = {result_subtract}")
+    print(f"{a} * {b} = {result_multiply}")
+    print(f"{a} / {b} = {result_divide}")
