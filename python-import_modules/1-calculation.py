a2eaaf17e70616c8b5a5e5a3f22cb39fc1b9803b
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-09-23 17:00:19 -0700

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    
+
+    from calculator_1 import add, sub, mul, div
+
     result_add = add(a, b)
     result_subtract = sub(a, b)
     result_multiply = mul(a, b)
