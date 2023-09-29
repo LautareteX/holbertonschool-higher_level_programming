561f5251ff95b5ab7155ea8153c02c26f4715913
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-09-29 12:55:20 -0700

    Task 0 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in range(len(new_list)):
-            new_list[search] = replace
+        new_list[search] = replace
 
-    return(new_list)
+    return (new_list)
