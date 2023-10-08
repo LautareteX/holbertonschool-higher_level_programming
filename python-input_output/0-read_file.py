a133ac1ed5bc2f86796c1e450865277e03ffc17d
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-10-08 14:44:53 -0700

    readme

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import os
 """now we are commenting)?"""
 
+
 def read_file(filename=""):
+    """Comments comments comments again"""
     with open(filename, mode="r", encoding="utf-8") as f:
         for line in f:
             print(line, end="")
