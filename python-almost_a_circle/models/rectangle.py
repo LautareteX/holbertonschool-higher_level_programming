8093748f9305a71f33e9c6d8b8cf63a43b881805
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-10-11 15:23:45 -0700

    task comments

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""I forgot to comment the code so I added this line """
+""" Rectangle class with methods """
 from base import Base
 
 
 class Rectangle(Base):
     """My Simple Rectangle Class"""
     def __init__(self, width=0, height=0, x=0, y=0, id=None):
-        """ Facu help me with this comment """
+        """ special method: Constructor. Initialize the attributes. """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,12 +15,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Here we are again"""
+        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Here we are again"""
+        """Set the width of the rectangle."""
         if type(value) is not int:
             raise TypeError(f"width  must be an integer")
         if value < 0:
@@ -29,12 +29,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Here we are again"""
+        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Here we are again"""
+        """Set the height of the rectangle."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -43,24 +43,24 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """Here we are again"""
+        """Get the x-coordinate of the position."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Here we are again"""
+        """Set the x-coordinate of the position."""
         if type(value) is not int:
             raise TypeError("x must be an integer")
         self.__x = value
 
     @property
     def y(self):
-        """Here we are again"""
+        """Get the y-coordinate of the position."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Here we are again"""
+        """Set the y-coordinate of the position."""
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -68,15 +68,17 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """Here we are again"""
+        """"Calculate and return the area of the rectangle."""
         return self.width * self.height
 
     def display(self):
-        """Here we are again"""
+        """Display the rectangle on the standard output."""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print('#' * self.width)
+            print(' ' * self.x + '#' * self.width)
 
     def __str__(self):
-        """Here we are again"""
+        """Get a string representation of the rectangle."""
         return (f'[Rectangle] ({id(self)}) {self.x}/{self.y} - '
                 f'{self.width}/{self.height}')
