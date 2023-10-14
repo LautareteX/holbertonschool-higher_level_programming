a114f74fbeae4be047cbd6e6d9db72de7ba04cb2
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-10-14 16:20:04 -0700

    task 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """I forgot to comment the code so I added this line """
+import json
 
 
 class Base():
@@ -14,3 +15,12 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Convert a list of dictionaries to Json format """
+        if not list_dictionaries:
+            return "[]"
+
+        json_data = json.dumps(list_dictionaries)
+        return json_data
