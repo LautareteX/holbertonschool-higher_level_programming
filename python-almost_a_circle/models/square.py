ae816c9eec6779181acc1993f46929a4aee2bbe5
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-10-14 09:48:43 -0700

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square class with methods """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """My Simple Square Class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Get a string representation of the rectangle."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return self.width
+
+    @size.setter
+    def size():
+        pass
