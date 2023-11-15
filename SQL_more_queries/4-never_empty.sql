e5ddc988005e1c6f77dfcbc9f1004cf2138d0b41
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-11-15 11:12:49 -0800

    4 & 5

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT default 1,
+    name VARCHAR(256)
+);
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
