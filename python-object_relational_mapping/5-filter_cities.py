023336286f26a290fe41adf85087210432531abe
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-11-19 11:54:32 -0800

    re task

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = '{}' RDER BY cities.id".format(sys.argv[4]))
+
+    q_rows = db_cursor.fetchall()
+    if q_rows is not None:
+        print(", ".join([x[0] for x in q_rows]))
+
+    db_cursor.close()
+    db.close()
